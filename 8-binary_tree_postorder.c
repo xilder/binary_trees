146bb904a89f6db79256115b3cff1f06c9fe0473
xilder <olabowaleayobami@gmail.com>
2023-10-31 09:14:28 -0700

    adds 8-binary_tree_postorder.c

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - traverses the binary tree in a postorderly fashion
+ *
+ * @tree: root node of the tree
+ * @func: a function to print a node's value
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	func(tree->n);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+}
