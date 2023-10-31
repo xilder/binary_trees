bd5e9bf751a2d242b82e3605bffad05bd4de2420
xilder <olabowaleayobami@gmail.com>
2023-10-31 08:22:51 -0700

    adds 5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,18 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - checks if a node is a leaf
+ * binary_tree_is_root - checks if a node is a root
  *
  * @node: the given node
  * Return: 1 if it is a leaf else 0
  */
 
-int binary_tree_is_leaf(const binary_tree_t *node)
+int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	if (node->left == NULL && node->right == NULL)
-		return (1);
-	else
+	if (node->left == NULL || node->right == NULL)
 		return (0);
+	else
+		return (1);
 }
