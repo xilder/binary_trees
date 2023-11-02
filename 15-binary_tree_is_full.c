7521ab520f5768bd7787e8293a8793ed69d0a80d
kofikoomson <kofikoomson94@gmail.com>
2023-11-01 19:19:10 -0700

    assignment 15 fix

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -5,37 +5,28 @@
  *
  * @tree: pointer to root
  *
- * Return: left and right node
+ * Return: 1 if the tree is full, 0 otherwise
  */
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int left = 0;
-	int right = 0;
-
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	else
+	
+	if (tree->left == NULL && tree->right == NULL)
 	{
-		if(tree->left && tree->right)
-		{
-			left = binary_tree_is_full(tree->left);
-			right = binary_tree_is_full(tree->right);
-			if (left == 0 || right == 0)
-			{
-				return (0);
-			}
-			return (1);
-		}
-		else if(!tree->left && !tree->right)
+		return (1);
+	}
+	
+	if (tree->left && tree->right)
+	{
+		int left = binary_tree_is_full(tree->left);
+		int right = binary_tree_is_full(tree->right);
+		if (left == 1 || right == 1)
 		{
 			return (1);
-		}
-		else
-		{
-			return(0)'
 		}
 	}
 }
