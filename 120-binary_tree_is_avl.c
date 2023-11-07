fe4a47e0f59294f2bfb9ffe8474c45b51012fadc
xilder <olabowaleayobami@gmail.com>
2023-11-07 11:52:45 -0800

    updates 120-binary_tree_is_avl.c

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a binary tree
+ * binary_tree_h - measures the height of a binary tree
  *
  * @tree: tree to measure the height of
  *
